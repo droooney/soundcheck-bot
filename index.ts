cbb432c26d697c889b28b7c3c5e1ba1c18e318ce
droooney <d.vor7@mail.ru>
2019-07-16 19:12:16 +0500

    add welcome message

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
+import * as qs from 'querystring';
 import * as util from 'util';
 import * as http from 'http';
 import Application = require('koa');
 import BodyParser = require('koa-bodyparser');
 import Router = require('koa-router');
+import axios from 'axios';
 
 util.inspect.defaultOptions.depth = 10;
 
@@ -10,24 +12,61 @@ const app = new Application();
 const server = http.createServer(app.callback());
 const router = new Router();
 
-interface Body {
-  type: string;
+interface Message {
+  user_id: number;
+  payload?: string;
 }
+
+interface ConfirmationBody {
+  type: 'confirmation';
+}
+
+interface NewMessageBody {
+  type: 'message_new';
+  object: Message;
+}
+
+type Body = ConfirmationBody | NewMessageBody;
+
+interface StartPayload {
+  command: 'start';
+}
+
+type ButtonPayload = StartPayload;
 
 router.post('/oajhnswfa78sfnah87hbhnas9f8', async (ctx) => {
   const body: Body = ctx.request.body;
-  const {
-    type
-  } = body;
 
   console.log('bot message', body);
 
-  if (type === 'confirmation') {
+  if (body.type === 'confirmation') {
     ctx.body = 'd48b8072';
+  } else if (body.type === 'message_new') {
+    let payload: ButtonPayload | null = null;
+
+    if (body.object.payload) {
+      try {
+        payload = JSON.parse(body.object.payload);
+      } catch (err) {}
+    }
+
+    if (payload && payload.command === 'start') {
+      const query = qs.stringify({
+        access_token: '45b653e39139ffec49a014720e9233e22c74adbccadc06a0224899fb5d3097697da3403ec6124efe9570a',
+        user_id: body.object.user_id,
+        message: 'Начнем!'
+      });
+      const {
+        data,
+        status
+      } = await axios.post(`https://api.vk.com/method/messages.send?${query}`);
+
+      console.log('message sent', status, data);
+    }
+
+    ctx.body = 'ok';
   } else {
-    ctx.body = {
-      success: true
-    };
+    ctx.body = 'ok';
   }
 });
 
